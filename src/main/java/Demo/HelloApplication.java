b866921966ece82ef5c2c51f829570996e2ad2a8
yoan <yoan.louvois@universite-paris-saclay.fr>
2025-01-24 08:33:09 +0100

    Interface gestion arbre yoan

--- src/main/java/Demo/HelloApplication.java
+++ src/main/java/Demo/HelloApplication.java
@@ -25,14 +25,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        /*
+
         //affichage samy
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/App/AssociationMember/LoginScreen.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        */
+
 
 
         //affichage yoan
@@ -48,7 +48,7 @@ public class HelloApplication extends Application {
         secondStage.setScene(scene1);
         secondStage.show();
 
-        /*
+
         //affichage ny
         Stage accueilStage = new Stage();
         FXMLLoader pageAccueilLoader = new FXMLLoader(getClass().getResource("/App/AssociationManagement/PageAccueil/PageAccueil.fxml"));
@@ -57,7 +57,7 @@ public class HelloApplication extends Application {
         accueilStage.setTitle("Gestion de l'association");
         accueilStage.setScene(accueilScene);
         accueilStage.show();
-*/
+
 
     }
     public static void MakeJsonFiles() {
