290df03ec7115eb2e7650647b797102a22b34d63
LeRoiDuRERb <thibault.lefebvre@universite-paris-saclay.fr>
2025-01-25 23:38:39 +0100

    Avancement du backend de donation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -35,5 +35,7 @@ module TreeApp {
     opens App.AssociationManagement.Controller.gestionMembres to com.fasterxml.jackson.databind, com.jfoenix, javafx.fxml;
     exports App.AssociationManagement.Controller.gestionVisiteArbre;
     opens App.AssociationManagement.Controller.gestionVisiteArbre to javafx.fxml, com.fasterxml.jackson.databind, com.jfoenix;
+    opens App.AssociationManagement.Controller.GestionDonation to javafx.fxml;
+
 
 }
